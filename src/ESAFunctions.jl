7b8b9a61bd34c0120500960ee57adc5dd9ed61a3
Natapon <w.natapon@gmail.com>
2023-08-15 08:08:40 +0200

    add plotseries_percentile

--- src/ESAFunctions.jl
+++ src/ESAFunctions.jl
@@ -4,11 +4,11 @@ module ESAFunctions
 import DataFrames, CategoricalArrays
 import SQLite, DBInterface, HDF5
 import PlotlyJS, Plots
-import Random, Distributions, Statistics
+import Random, Distributions, Statistics, StatsBase
 
 # Declare export ==============================================================
 # Visualization functions
-export plottimeseries, plotbar, plothistogram, plotcontour, plotheatmap, plotvolume, plotcluster
+export plottimeseries, plotbar, plothistogram, plotcontour, plotheatmap, plotvolume, plotcluster, plotseries_percentile
 # Data interface functions
 export save_dftoh5, load_h5todf, loadall_h5todf, save_dftodb, load_dbtodf, list_dbtable, appendtxt
 # Profile modification functions
@@ -247,6 +247,35 @@ function plotcluster(data::Matrix, label::Vector)
             Plots.plot!(p, data[1, idx], data[2, idx], seriestype=:scatter, label=string(l), opacity=0.75)
         end
     end
+    return p
+end
+
+"""
+    plotseries_percentile(mtx::Matrix; xlab, ylab::String, title, bsort::Bool=false, linealpha=0.10, ylims)
+
+Plot multiple timeseries with the same length from `mtx`, a matrix of (time x observation)
+
+# Keyword Arguments
+- `bsort` : sort the series
+- `linealpha` : transparency of the original series
+"""
+function plotseries_percentile(mtx::Matrix; xlab::String="Hours in a year", ylab::String="Power(kW)", title::String="", bsort::Bool=false, linealpha=0.10, ylims=:auto)
+    pf_pct50 = Statistics.mean(mtx, dims=2)[:]
+    pf_pct25 = zero(pf_pct50)
+    pf_pct75 = zero(pf_pct50)
+    for idx ∈ 1:length(pf_pct50)
+        pf_pct25[idx] = StatsBase.quantile(mtx[idx, :], 0.25)
+        pf_pct75[idx] = StatsBase.quantile(mtx[idx, :], 0.75)
+    end
+    if bsort
+        sort!(pf_pct50); sort!(pf_pct25); sort!(pf_pct75)
+        mtx = sort(mtx, dims=1)
+    end
+    p = Plots.plot(title=title, xlabel=xlab, ylabel=ylab, ylims=ylims)
+    [Plots.plot!(p, sort(mtx[:, iobj]), label=nothing, linecolor=:black, linealpha=linealpha) for iobj ∈ 1:(size(mtx)[2])]
+    Plots.plot!(p, pf_pct50, label="pct50", linecolor=Plots.palette(:default)[1])
+    Plots.plot!(p, pf_pct75, label="pct75", linecolor=Plots.palette(:default)[2])
+    Plots.plot!(p, pf_pct25, label="pct25", linecolor=Plots.palette(:default)[3])
     return p
 end
 
